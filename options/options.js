855845918c1154705d481377adcd947f30231505
Crowdbotics <team@crowdbotics.com>
2023-05-09 09:50:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mongo-check-40573",
+  url: "https://mongo-check-40573.botics.co",
+  api: "https://mongo-check-40573.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
